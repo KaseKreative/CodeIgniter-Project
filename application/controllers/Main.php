a12846b0692b42b657a31732b04a891aab54e30b
KaseKreative <KaseKreative@fullsail.edu>
2015-05-16 01:49:13 -0400

    Profile Controller

--- application/controllers/Main.php
+++ application/controllers/Main.php
@@ -1,7 +1,7 @@
 <?php
 defined('BASEPATH') OR exit('No direct script access allowed');
 
-class Index extends CI_Controller {
+class Main extends CI_Controller {
 
 	/**
 	 * Index Page for this controller.
@@ -18,7 +18,7 @@ class Index extends CI_Controller {
 	 * map to /index.php/welcome/<method_name>
 	 * @see http://codeigniter.com/user_guide/general/urls.html
 	 */
-	public function index()
+	public function Index()
 	{
     echo "Hello World!";
 		// $this->load->view('welcome_message');
---
+++ application/controllers/profile.php
@@ -0,0 +1,11 @@
+<?php
+defined('BASEPATH') OR exit('No direct script access allowed');
+
+class Profile extends CI_Controller {
+
+	public function Profile()
+	{
+    echo "Hello World! This is the Profile";
+		// $this->load->view('welcome_message');
+	}
+}
